ac3edfcb9d094b5be954333f85af836545840901
ogjop <ogjop@192.168.3.12>
2023-02-09 22:29:11 +0900

    オブジェクト指向プログラミング1学習終了

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,35 @@
+package baseball;
+
+public class BaseBallTeam {
+
+	public String name;
+	public int win;
+	public int lose;
+	public int draw;
+
+    public BaseBallTeam() {
+ 	}
+
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+    	this.name = name;
+    	this.win  =  win;
+    	this.lose = lose;
+    	this.draw = draw ;
+    }
+
+    public double getRate() {
+		double result = (double)win / (win + lose);
+		return result;
+}
+
+	public void report() {
+	    System.out.println
+		(this.name + "の2022年の成績は" + this.win + "勝" + this.lose
+		 + "敗" + this.draw + "分" + "勝率は" + getRate() + "です。");
+	}
+}
+
+
+
+
+
